f0480ab11294d0006a60de43d611118496a95683
brreex <berhanuasa@gmail.com>
2017-09-10 17:08:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserEventExperimentComponent } from './browser-event-experiment/browser-event-experiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserEventExperimentComponent
   ],
   imports: [
     BrowserModule
